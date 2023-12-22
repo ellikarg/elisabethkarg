92d92ded8620b2f54bfc2b94167b8f961d86541f
elli karg <elisa.karg.bogor@gmail.com>
2023-12-22 23:40:44 +0000

    feat: add Introduction page to App.js

---
+++ elisabethkarg/src/pages/Introduction.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../index.css'
+
+export const Introduction = () => {
+  return (
+    <>
+    <div className='Background'>
+    <div className='Name'></div>
+    <div className='Banner'>
+        <ul className='Navbar'>
+            <li>About</li>
+            <li>Background</li>
+            <li>Journal</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    </div>    
+    </>
+  )
+}
